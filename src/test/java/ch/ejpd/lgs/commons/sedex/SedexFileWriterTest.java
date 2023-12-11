fe15475b8e845b327e4524da414b816d3c4ac68f
katanasov <kiril.atanasov@adesso.bg>
2023-12-11 20:12:32 +0200

    add landRegion In Sedex Payload

--- src/test/java/ch/ejpd/lgs/commons/sedex/SedexFileWriterTest.java
+++ src/test/java/ch/ejpd/lgs/commons/sedex/SedexFileWriterTest.java
@@ -9,8 +9,12 @@ import ch.ejpd.lgs.searchindex.client.entity.type.JobType;
 import ch.ejpd.lgs.searchindex.client.model.JobCollectedPersonData;
 import ch.ejpd.lgs.searchindex.client.model.JobMetaData;
 import ch.ejpd.lgs.searchindex.client.model.ProcessedPersonData;
+
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -21,6 +25,8 @@ import java.util.UUID;
 import java.util.stream.Collectors;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
+
+import ch.ejpd.lgs.searchindex.client.service.exception.WritingSedexFilesFailedException;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
@@ -28,8 +34,9 @@ class SedexFileWriterTest {
   private static final String PERSON_DATA_PREFIX = "GBPersonEvent-";
   private static final String PERSON_DATA_SUFFIX = ".json";
   private static final String SENDER_ID_A = "LGS-123-AAA";
-
+  private static final String EXPECTED_SENDER_ID_IN_METADATA = "\"landRegister\":\"LGS-123-AAA\"";
   private static final String TEST_DIR = "/tmp/lwgs-sedex-test";
+  private static final String METADATA_FILE_NAME = "metadata.json";
 
   private final UUID messageId = UUID.randomUUID();
   private final SedexFileWriter sedexFileWriter = new SedexFileWriter(Paths.get(TEST_DIR), true);
@@ -103,6 +110,7 @@ class SedexFileWriterTest {
                     .type(JobType.FULL)
                     .pageNr(0)
                     .isLastPage(true)
+                    .landRegister(SENDER_ID_A)
                     .build()));
 
     assertTrue(sedexFileWriter.sedexDataFile(messageId).exists());
@@ -115,6 +123,40 @@ class SedexFileWriterTest {
     assertTrue(files.contains(PERSON_DATA_PREFIX + transactionTwo + PERSON_DATA_SUFFIX));
   }
 
+  @Test
+  void testLandRegionInSedexPayload() throws WritingSedexFilesFailedException {
+    final UUID transactionId = UUID.randomUUID();
+    final UUID jobId = UUID.randomUUID();
+
+    sedexFileWriter.writeSedexPayload(
+            messageId,
+            JobCollectedPersonData.builder()
+                    .senderId(SENDER_ID_A)
+                    .jobId(jobId)
+                    .processedPersonDataList(
+                            Arrays.asList(
+                                    ProcessedPersonData.builder()
+                                            .senderId(SENDER_ID_A)
+                                            .transactionId(transactionId)
+                                            .payload("{}")
+                                            .build()))
+                    .messageId(messageId)
+                    .page(0)
+                    .build(),
+            JobMetaData.builder()
+                    .jobId(jobId)
+                    .type(JobType.FULL)
+                    .pageNr(0)
+                    .isLastPage(true)
+                    .landRegister(SENDER_ID_A)
+                    .build());
+
+    File file = sedexFileWriter.sedexDataFile(messageId);
+    String content = getContentOfEntryInZip(METADATA_FILE_NAME, file);
+
+    assertTrue(content.contains(EXPECTED_SENDER_ID_IN_METADATA));
+  }
+
   private List<String> getZipContentFileList(File zipFile) {
     try {
       ZipFile zf = new ZipFile(zipFile);
@@ -123,5 +165,19 @@ class SedexFileWriterTest {
       e.printStackTrace();
     }
     return null;
+  }
+
+  private String getContentOfEntryInZip(String entryName, File zipFile) {
+      try {
+          ZipFile zf = new ZipFile(zipFile);
+          ZipEntry entry = zf.getEntry(entryName);
+          InputStream stream = zf.getInputStream(entry);
+
+          return new BufferedReader(new InputStreamReader(stream))
+                  .lines().parallel().collect(Collectors.joining("\n"));
+      } catch (IOException e) {
+          e.printStackTrace();
+          throw new RuntimeException(e);
+      }
   }
 }
